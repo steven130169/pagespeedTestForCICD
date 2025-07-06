5b1be0110791751c36e10d21b1af68df8fab7508
Steven Cheng <steven.cheng1@hp.com>
2025-07-06 09:52:23 +0800

    Add github action and update node version

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const chromeLauncher = require('chrome-launcher');
 (async () => {
   const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
   const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
-  const runnerResult = await lighthouse('https://example.com', options);
+  const runnerResult = await lighthouse('https://mall.iopenmall.tw/iopen/', options);
 
   // `.report` is the HTML report as a string
   const reportHtml = runnerResult.report;
+  const performanceScore = runnerResult.lhr.categories.performance.score * 100;
   console.log('Report is done for', runnerResult.lhr.finalUrl);
-  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  console.log('Performance score was', performanceScore);
 
   await chrome.kill();
+
+  if (performanceScore < 80) {
+    console.error('Lighthouse performance score is less than 80.');
+    process.exit(1);
+  }
 })();
