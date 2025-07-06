692172d28781eac2598cbe00aada71a880ff1290
Steven Cheng <steven.cheng1@hp.com>
2025-07-06 09:44:18 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+(async () => {
+  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+  const runnerResult = await lighthouse('https://example.com', options);
+
+  // `.report` is the HTML report as a string
+  const reportHtml = runnerResult.report;
+  console.log('Report is done for', runnerResult.lhr.finalUrl);
+  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+  await chrome.kill();
+})();
